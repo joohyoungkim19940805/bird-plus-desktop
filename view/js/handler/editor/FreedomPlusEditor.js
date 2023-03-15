48bddabb5934d41dafad0c834ccb0e604f5ac67e
joohyoungkim19940805 <oozu1994@gmail.com>
2023-03-16 07:31:11 +0900

    채팅 에디터 테스트 코드 작성

--- view/js/handler/editor/FreedomPlusEditor.js
+++ view/js/handler/editor/FreedomPlusEditor.js
@@ -22,7 +22,7 @@ export default class FreedomEditorPlus extends HTMLDivElement {
 		
 		static #checkSuperClass(originClazz, newClazz){
 			let check = Object.getPrototypeOf(newClazz);
-			while(true){
+			while(check != undefined){
 				if(originClazz == undefined){
 					throw new Error(`${newClazz.prototype.constructor.name} is not extends in${originClazz.prototype.constructor.name}`)
 				}else if(check == originClazz){
@@ -52,6 +52,9 @@ export default class FreedomEditorPlus extends HTMLDivElement {
             this.#isLoaded = true;
 			this.contentEditable = true;
 			let line = new Line();
+			line.abcd = this;
+			line.onkeyup = (event)=>console.log(event)
+			console.log(line.abcd);
 			this.append(line);
 			// 최초에 첫번째 Line 엘레멘탈의 height 값이 0인 경우 최소 사이즈 지정
 			if(parseInt(window.getComputedStyle(line).height) == 0){
@@ -64,6 +67,13 @@ export default class FreedomEditorPlus extends HTMLDivElement {
 					}
 				})
 			}
+			// 온키업인 경우 엔터로 얻는 엘레멘탈을 이어받나? = 아니었음
+			this.onkeyup = (event) => {
+				console.log(window.getSelection());
+			}
+			this.onselectstart  = (event) => console.log(event)
+			this.onmouseup = (event) => console.log(window.getSelection())
+			// getSelection - isCollapsed이 true인 경우 선택 된 텍스트가 없음, false = 있음
 		}
 	}
 	disconnectedCallback(){
--- view/js/handler/editor/component/Line.js
+++ view/js/handler/editor/component/Line.js
@@ -8,15 +8,36 @@ export default class Line extends HTMLDivElement {
 		console.log(option);
 	}
 	connectedCallback(){
+		console.log(this.dataset.test_abc)
 		if( ! this.#isLoaded){
+			this.draggable="true"
             this.#isLoaded = true;
 			this.classList.add('freedom-line')
-			this.style.minHeight = '1em';
 			this.dataset.test = 'test';
+			//this.onselectstart  = (event) => console.log(event)
+			//this.onselectionchange = (event) => this.selectionchangeEventFunction(event);
+			//this.onselect = (event) => console.log(event);
+			//this.onbeforexrselect = (event) => console.log(event)
+			//this.onmouseup = (event) => console.log(window.getSelection())
+			let testDiv = document.createElement('div');
+			testDiv.className = 'testAAAA'
+			testDiv.textContent = 'testaaa';
+			this.append(testDiv);
+			//자기 자신 호출로 아웃오브메모리
+			//this.parentElement.append(new Line());
 		}
 	}
 	disconnectedCallback(){
         this.#isLoaded = false;
     }
+
+	selectstartEventFunction(event){
+		console.log(event)
+		console.log(window.getSelection())
+		console.log(document.getSelection())
+	}
+	selectionchangeEventFunction(event){
+		console.log(event);
+	}
 
 }
