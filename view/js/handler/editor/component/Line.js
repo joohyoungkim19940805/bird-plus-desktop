09f56c0e9a71193e564fa9ccdc85dc480c413a18
joohyoungkim19940805 <oozu1994@gmail.com>
2023-03-23 17:55:56 +0900

    에디터 서식 일부 개발 중

--- view/js/handler/editor/component/Line.js
+++ view/js/handler/editor/component/Line.js
@@ -2,7 +2,13 @@
 export default class Line extends HTMLDivElement {
 	#isLoaded = false;
 	#prevParent;
-	static extendsElement = 'div'
+	/**
+	 * line은 무조건 div를 상속받아야 합니다.
+	 * mark라던가 span으로 사용해도 엔터치는 순간 div로 감싸입니다. 
+	 * 즉 엔터시 무조건 div로 됩니다.
+	 * 추후 엔터 이벤트를 막고 알트 엔터로 트리거 할 수 있도록 바꾸어야합니다.
+	 */
+	static #extendsElement = 'div'
 	constructor(option={}){
 		super();
 		console.log(option);
