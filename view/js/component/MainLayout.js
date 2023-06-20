bda84ede9ded0117c0d2c6bddc6a55f3ad210538
joohyoungkim19940805 <oozu1994@gmail.com>
2023-06-20 19:14:17 +0900

    리사이즈 패널 재작업

--- view/js/component/MainLayout.js
+++ view/js/component/MainLayout.js
@@ -2,54 +2,224 @@
  * 커스텀 메인 레이아웃
  */
 
-class MainLayout extends HTMLElement {
+class FlexLayout extends HTMLElement {
+	
+	static #componentName = 'flex-layout'
+	static #parentClass = 'flux-layout-wrap';
+	static #childClass = 'flux-layout-content';
+	static #resizePanelClass = 'flex-resize-panel';
+	static #defaultStyle = Object.assign(document.createElement('style'), {
+		id: 'flex-layout-style',
+		textContent : `
+			${this.#componentName}[data-direction="column"]{
+				flex-direction: column;
+			}
+			${this.#componentName}[data-direction="row"]{
+				flex-direction: row;
+			} 
+			${this.#componentName} > ${this.#childClass}[data-is_resize="true"]{
+				flex: 1 1 0%;
+			}
+			${this.#componentName} > ${this.#childClass}[data-is_resize="false"]{
+				flex: 0 0 0%;
+			}
+			${this.#componentName} .${this.#resizePanelClass}{
+				background-color: #b1b1b1;
+				z-index: 0;
+				position: absolute;
+				display: flex;
+				justify-content: center;
+				flex : 0 0 0%;
+				-moz-user-select: -moz-none;
+				-khtml-user-select: none;
+				-webkit-user-select: none;
+				-ms-user-select: none;
+				user-select: none;
+				right: 0;
+				bottom: 0;
+			}
+			${this.#componentName}[data-direction="row"] .${this.#resizePanelClass}{
+				width: 2.5px;
+				height: 100%;
+			}
+			${this.#componentName}[data-direction="column"] .${this.#resizePanelClass}{
+				height: 2.5px;
+				width:100%;
+			}
+			${this.#componentName}[data-direction="row"] .${this.#resizePanelClass}:hover > .hover{
+				width: 5px;
+				height: inherit;
+			}
+			${this.#componentName}[data-direction="column"] .${this.#resizePanelClass}:hover > .hover{
+				height: 5px;
+				width: inherit;
+			}
+			${this.#componentName}[data-direction="row"] .${this.#resizePanelClass}{
+				cursor: ew-resize;
+			}
+			${this.#componentName}[data-direction="column"] .${this.#resizePanelClass}{
+				cursor: ns-resize;
+			}
+			${this.#componentName} .${this.#resizePanelClass} > .hover{
+				display: none;
+			}
+			${this.#componentName} .${this.#resizePanelClass}:hover{
+				animation-name: drag-panel-hover;
+				animation-fill-mode: forwards;
+				animation-direction: normal;
+				animation-duration: 0.3s;
+				animation-iteration-count: 1;
+				animation-timing-function: cubic-bezier(1, -0.37, 0.73, 0.8);
+			}
+			
+			${this.#componentName} .${this.#resizePanelClass}:hover > .hover{
+				background-color: #0066ffb5;
+				z-index: 9999;
+				position: absolute;
+				display: block;
+				animation-name: drag-panel-hover-highlight;
+				animation-fill-mode: forwards;
+				animation-direction: normal;
+				animation-duration: 0.3s;
+				animation-iteration-count: 1;
+				animation-timing-function: cubic-bezier(1, -0.37, 0.73, 0.8);
+			}
+			@keyframes drag-panel-hover {
+				0%{background-color: #b1b1b1d6;}
+				10%{background-color: #b1b1b1ba;}
+				20%{background-color: #b1b1b194;}
+				40%{background-color: #b1b1b17d;}
+				60%{background-color: #b1b1b152;}
+				80%{background-color: #b1b1b130;}
+				100%{background-color: #b1b1b100;animation-play-state: paused;}
+			}
+			@keyframes drag-panel-hover-highlight {
+				0%{opacity: 0;}
+				10%{opacity: 0.1;}
+				20%{opacity: 0.2;}
+				40%{opacity: 0.4;}
+				60%{opacity: 0.6;}
+				80%{opacity: 0.8;}
+				100%{opacity: 1;animation-play-state: paused;}
+		`
+	});
+
+	static{
+		document.head.append(this.#defaultStyle);
+	}
+
+	static get componentName(){
+		return this.#componentName;
+	}
+	static get parentClass(){
+		return this.#parentClass;
+	}
+	static get childClass(){
+		return this.#childClass;
+	}
+	static get resizePanelClass(){
+		return this.#resizePanelClass;
+	}
+
 	#isLoaded = false;
-	#isAddEventDone = false;
+	#growLimit = 0; 
+	//#shrinkDefault = 1;
+	//#basisDefault = '0%';
+	
+	// 가로 모드인 경우 lastElementChild의 리사이즈 제거 필요
+	// 세로 모드인 경우 firstElementChild의 리사이즈 제거 필요 
 	constructor(){
 		super();
+		let observer = new MutationObserver( (mutationList, observer) => {
+			mutationList.forEach((mutation) => {
+				let {addedNodes, removedNodes} = mutation;
+				addedNodes.forEach(childElement=>{
+					if(childElement.nodeType != Node.ELEMENT_NODE){
+						return;
+					}
+					
+					childElement.classList.add(FlexLayout.childClass)
+					
+					if(childElement.dataset.is_resize == 'true'){
+						let resizePanel = this.#createResizePanel();
+						childElement.__resizePanel = resizePanel;
+						childElement.after(resizePanel);
+						resizePanel.__resizeTarget = childElement; 
+					}
+				});
+				this.#growLimit = [...this.children].filter(e=>e.dataset.is_resize == 'true').length;
+			});
+		})
+		observer.observe(this, {childList:true});
 	}
 
 	connectedCallback(){
         if( ! this.#isLoaded){
 			this.#isLoaded = true;
+			/*
+			document.addEventListener('DOMContentLoaded', (event) => {
+
+			})
+			*/
         }
-		if( ! this.#isAddEventDone ){
-			this.addMainEvent();
-			this.#isAddEventDone = true;
-		}
     }
+
 	disconnectedCallback(){
         this.#isLoaded = false;
     }
 
-	addMainEvent(){
-		let header = document.querySelector('header-default > #header_content');
-		this.style.height = (window.outerHeight - header.clientHeight) + 'px'
-		this.style.width = window.outerWidth + 'px'; 
-		window.myAPI.electronEventTrigger.addElectronWindowEventListener('resized', ( [width, height] ) => {
-			let mathHeight =  (window.outerHeight - header.clientHeight);
-			//mathHeight = mathHeight - (mathHeight * 0.02)
-			this.style.height = mathHeight + 'px'
-			this.style.width = window.outerWidth + 'px'; 
+	#createResizePanel (){
+		// panel_width가 반드시 필요한지 확인 할 것 2023 06 20
+		let resizePanel = Object.assign(document.createElement('div'), {
+			className: FlexLayout.resizePanelClass,
+			innerHTML : `
+				<div class="hover">
+				</div>
+				<div class="panel_width">
+				</div>
+			`
 		});
-		/*window.addEventListener('load',()=>{
-			let mathHeight =  (window.outerHeight - header.clientHeight);
-			this.style.height = mathHeight + 'px'
-			this.style.width = window.outerWidth + 'px';
-		})*/
-		/*
-		[...this.children].forEach(e=>{
-			let originWidth = window.getComputedStyle(e).width;
-			e.style.width = originWidth
-			let originRatio = parseInt(originWidth) / window.outerWidth
-			window.myAPI.electronEventTrigger.addElectronWindowEventListener('resized', ( [width, height] ) => {
-				let newWidth = (window.outerWidth * originRatio);
-				e.style.width = newWidth + 'px';
-				originRatio = newWidth / window.outerWidth
-			});
-		})
-		*/
+		return resizePanel;
 	}
+
+	/**
+	 * 
+	 * @param {HTMLElement} resizePanel 
+	 */
+	#addResizePanelEvent(resizePanel){
+		let resizeCursor;
+		if(this.dataset.direction == 'row'){
+			resizeCursor = 'ew-resize';
+		}else if(this.dataset.direction == 'column'){
+			resizeCursor = 'ns-resize';
+		}
+		resizePanel.onmousedown = (event) => {
+			resizePanel.setAttribute('data-is_mouse_down', '');
+			document.body.style.cursor = 'ew-resize'
+		}
+
+		window.addEventListener('mouseup', (event) => {
+			if( resizePanel.hasAttribute('data-is_mouse_down')) {
+				resizePanel.removeAttribute('data-is_mouse_down');
+			}
+			
+			if(document.body.style.cursor == 'ew-resize' || document.body.style.cursor == 'ns-resize'){
+				document.body.style.cursor = '';
+			}
+		})
+		
+		resizePanel.onmouseup = (event) => {
+			resizePanel.removeAttribute('data-is_mouse_down');
+		}
+
+		window.addEventListener('mousemove', (event) => {
+			if( ! this.hasAttribute('data-is_mouse_down') || ! resizePanel.__resizeTarget ){
+				return;
+			}
+		})
+	}
+
 }
 
-window.customElements.define('main-layout', MainLayout);
+
+window.customElements.define(FlexLayout.name, MainLayout);
